06350446fee2b99b4ac93008eb9fb0debd02fc2b
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-15 09:49:50 -0200

    Criação das tabelas no banco

---
+++ 03-JPA-introducao/src/br/com/fiap/teste/Teste.java
@@ -0,0 +1,25 @@
+package br.com.fiap.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// Criar um gerenciador de entidades
+		// Primeiro cria a fábrica
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("CLIENTE_ORACLE");
+
+		// Depois, a fabrica cria os Entity Manager
+		EntityManager em = fabrica.createEntityManager();
+
+		
+		
+		
+		em.close();
+		fabrica.close();
+
+	}
+
+}
